6abd003dfa5b1d5593aded4a1c21886d30f0d728
Richard Kichenama <rkichenama@gmail.com>
2017-08-04 21:42:45 -0400

    starting fresh

--- cv/jsx/Interest.jsx
+++
@@ -1,5 +0,0 @@
-const Interest = ({name = ''}) => (
-  <span className={'interest'}>
-    { name }
-  </span>
-)
--- cv/jsx/utilities.jsx
+++
@@ -1,6 +0,0 @@
-const pad = (n, space = 2, padChar = '0') => {
-  if (n == undefined) return '';
-  n = n.toString();
-  if (n.length >= space) { return n; }
-  else { return pad(padChar + n, space, padChar); }
-}
